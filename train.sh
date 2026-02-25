9eda0990614638c985b166d9f8892fd39e600403
Alfred171 <49404174+alfred141@users.noreply.github.com>
2026-02-25 14:58:27 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,37 @@
+# ==== Model settings ====
+# adaptation {finetune,lp}
+#!/bin/bash
+
+CSV=experiments.csv
+
+tail -n +2 $CSV | while IFS=',' read MODEL MODEL_ARCH FINETUNE DATASET
+do
+  name=$(basename $DATASET)_${FINETUNE%%.*}
+
+  echo "🚀 $MODEL | $MODEL_ARCH | $FINETUNE | $DATASET"
+
+ADAPTATION="finetune"
+
+# ==== Data settings ====
+# change the dataset name and corresponding class number
+DATASET="$DATASET"
+DATA_PATH="./Data/${DATASET}"
+NUM_CLASS=5
+task="${MODEL_ARCH}_${FINETUNE}_${DATASET}_${ADAPTATION}"
+
+CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=1 --master_port=48765 main_finetune.py \
+  --model "${MODEL}" \
+  --model_arch "${MODEL_ARCH}" \
+  --finetune "${FINETUNE}" \
+  --savemodel \
+  --global_pool \
+  --batch_size 24 \
+  --world_size 1 \
+  --epochs 50 \
+  --nb_classes "${NUM_CLASS}" \
+  --data_path "${DATA_PATH}" \
+  --input_size 224 \
+  --task "${task}" \
+  --adaptation "${ADAPTATION}"
+  --device "cuda:0"
+done
